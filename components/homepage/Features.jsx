8536b5d26065cebe1b362d8293893cc45493e070
Hrishav <hrishav.kumar@harness.io>
2022-10-25 09:42:24 +0530

    Updated Landing Page and added Enterprise Page (#58)
    * Updated Landing Page and Enterprise Page
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    * Fixed BLC Error
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    * Updated terminologies
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    * fix: Updated empty get started link and added upcoming event
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- components/homepage/Features.jsx
+++ components/homepage/Features.jsx
@@ -1,52 +1,53 @@
 import { TextLink } from "@includes/CTA"
 import styles from "@includes/scss/Features.module.scss"
 import { BodyHead, Paragraph } from "@includes/Texts"
-import { SkewedContainerFluid } from "@layouts/Container"
+import { Container, ContainerFluid } from "@layouts/Container"
 import { featureUtils } from "./utils/featureUtils"
 
 const FeatureCard = ({ feature }) => {
   return (
-    <div className={styles.featureCard} style={{ background: "#231a47" }}>
+    <div className={styles.featureCard}>
       {feature.svg ?? ""}
-      <h3 className="text-xl md:text-2xl text-white font-medium mt-4 mb-2">
+      <h3 className="text-xl md:text-2xl text-primary font-medium mt-4 mb-2">
         {feature.title}
       </h3>
-      <Paragraph dark>{feature.content}</Paragraph>
+      <Paragraph hint>{feature.content}</Paragraph>
     </div>
   )
 }
 
 const Features = () => {
   return (
-    <SkewedContainerFluid color="#1C0732">
-      <BodyHead dark className="text-center md:text-left">
-        Loaded with features to{" "}
-        <span className="text-accent md:block">achieve resilience</span>
-      </BodyHead>
-      <Paragraph dark className="md:max-w-3xl mt-4 text-center md:text-left">
-        Over time, with the monthly cadence releases and community engagement,
-        we have added a lot of features and made LitmusChaos much easier for the
-        end-users. With the launch of Litmus 2.0, a new way of chaos engineering
-        can be performed by the users.
-      </Paragraph>
-      <div className={styles.featureCardCont}>
-        {featureUtils.map((feature, index) => {
-          return <FeatureCard key={feature.title + index} feature={feature} />
-        })}
-        <div className={`${styles.featureCard} ${styles.cardBackImg}`}>
-          <h3 className="text-xl md:text-2xl text-success font-medium mt-4 mb-2">
-            And many more features
-          </h3>
-          <TextLink
-            href="https://docs.litmuschaos.io/docs/introduction/features/"
-            external
-            dark
-          >
-            View more features
-          </TextLink>
+    <ContainerFluid className="bg-[#FAFBFC]">
+      <Container className="md:pt-36 md:pb-28 pt-16 pb-14">
+        <BodyHead className="text-center md:text-left">
+          Loaded with features to{" "}
+          <span className="text-accent md:block">achieve resilience</span>
+        </BodyHead>
+        <Paragraph className="md:max-w-3xl mt-4 text-center md:text-left">
+          Over time, with the monthly cadence releases and community engagement,
+          we have added a lot of features and made LitmusChaos much easier for
+          the end-users. With the launch of Litmus 2.0, a new way of chaos
+          engineering can be performed by the users.
+        </Paragraph>
+        <div className={styles.featureCardCont}>
+          {featureUtils.map((feature, index) => {
+            return <FeatureCard key={feature.title + index} feature={feature} />
+          })}
+          <div className={`${styles.featureCard} ${styles.cardBackImg}`}>
+            <h3 className="text-xl md:text-2xl text-success font-medium mt-4 mb-2">
+              And many more features
+            </h3>
+            <TextLink
+              href="https://docs.litmuschaos.io/docs/introduction/features/"
+              external
+            >
+              View more features
+            </TextLink>
+          </div>
         </div>
-      </div>
-    </SkewedContainerFluid>
+      </Container>
+    </ContainerFluid>
   )
 }
 
--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,179 +1,60 @@
-import { GithubButton } from "@components/SVG/NavbarSVG"
+import { GithubButton, SlackSVG } from "@components/SVG/NavbarSVG"
 import { OutlinedButton, RegularButton } from "@includes/CTA"
 import styles from "@includes/scss/Hero.module.scss"
 import { HeroHead, Paragraph } from "@includes/Texts"
-import { Container } from "@layouts/Container"
-import Link from "next/link"
-import { useEffect, useState } from "react"
+import { Container, ContainerFluid } from "@layouts/Container"
 
 const Hero = () => {
-  const url = "https://api.github.com/repos/litmuschaos/litmus"
-  const [gitHub, setGitHub] = useState({
-    stars: "",
-    watchers: "",
-    forks: ""
-  })
-
-  useEffect(() => {
-    ;(async () => {
-      await fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          setGitHub({
-            stars: data.stargazers_count,
-            watchers: data.subscribers_count,
-            forks: data.forks_count
-          })
-        })
-    })()
-  }, [])
-
-  const GitHubButton = ({ icon, name, count, href }) => {
-    return (
-      <div className={styles.gitPart}>
-        <Link href={href}>
-          <a target="_blank" rel="noopener noreferrer">
-            <div className={styles.gitLink}>
-              <img src={`/landing_images/hero/${icon}.svg`} alt="LitmusChaos" />
-              <span>{name}</span>
-            </div>
-          </a>
-        </Link>
-        <div className={styles.gitCount}>
-          <span>{count}</span>
-        </div>
-      </div>
-    )
-  }
-
   return (
-    <Container className={styles.heroCont}>
-      <div className={styles.textCont}>
-        <div className={styles.incubationBar}>
-          <div className={styles.gradBar}></div>
-          <div className={styles.itemCont}>
-            <span className={styles.incubationText}>
-              <svg
-                width={21}
-                height={27}
-                viewBox="0 0 21 27"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M21 0.245117H0V24.7663C0 25.8644 0.881439 26.7545 1.96875 26.7545H19.0312C20.1186 26.7545 21 25.8644 21 24.7663V0.245117Z"
-                  fill="#878EDE"
-                />
-                <path
-                  d="M3.23096 3.47754H6.46173V10.9131H3.23096V3.47754Z"
-                  fill="#4028A0"
-                />
-                <path
-                  d="M3.23096 10.2666H6.46173V23.5213H3.23096V10.2666Z"
-                  fill="white"
-                />
-                <path
-                  d="M8.07715 20.2891H17.7695V23.5219H8.07715V20.2891Z"
-                  fill="white"
-                />
-              </svg>
-              Litmus is in incubation
-            </span>
-            <span className={styles.divider}></span>
-            <span className={styles.incubationLink}>
-              <Link href="https://www.cncf.io/blog/2022/01/11/litmuschaos-becomes-a-cncf-incubating-project/">
-                <a
-                  className="flex items-center text-xs md:text-sm font-medium text-litmus gap-1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  See Announcement
-                  <svg
-                    width={22}
-                    height={21}
-                    viewBox="0 0 22 21"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.72705 10.5645H17.9264"
-                      stroke="#5B44BA"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M12.1177 4.93945L17.9265 10.5645L12.1177 16.1895"
-                      stroke="#5B44BA"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </a>
-              </Link>
-            </span>
+    <ContainerFluid className="bg-[#FAFBFC]">
+      <Container className={styles.heroCont}>
+        <div className={styles.textCont}>
+          <HeroHead hero>
+            <span className="block">Open Source</span>
+            <span className="block">Chaos Engineering</span>
+            <span className="block">platform</span>
+          </HeroHead>
+          <Paragraph hint className="mt-6 text-center lg:text-left">
+            Litmus is an open source Chaos Engineering platform that enables
+            teams to identify weaknesses & potential outages in infrastructures
+            by inducing chaos tests in a controlled way.
+          </Paragraph>
+          <Paragraph hint className="mt-4 text-center lg:text-left">
+            Developers & SREs can simply execute Chaos Engineering with Litmus
+            as it is easy to use, based on modern chaos engineering practices &
+            community collaborated. Litmus is 100% open source & CNCF-hosted.
+          </Paragraph>
+          <div className="mt-4 flex gap-4 justify-center lg:justify-start">
+            <RegularButton
+              href="https://github.com/litmuschaos/litmus"
+              external
+            >
+              <span className="flex items-center">
+                <GithubButton />
+                Get Started
+              </span>
+            </RegularButton>
+            <OutlinedButton
+              external
+              href="https://kubernetes.slack.com/?redir=%2Farchives%2FCNXNB0ZTN"
+            >
+              <span className="flex items-center">
+                <SlackSVG />
+                Join Slack
+              </span>
+            </OutlinedButton>
           </div>
         </div>
-        <HeroHead hero>
-          <span className="block">Cloud Native</span>
-          <span className="block">Chaos Engineering</span>
-          <span className="block">platform</span>
-        </HeroHead>
-        <Paragraph hint className="mt-6 text-center lg:text-left">
-          Litmus is an open source Chaos Engineering platform that enables teams
-          to identify weaknesses & potential outages in infrastructures by
-          inducing chaos tests in a controlled way.
-        </Paragraph>
-        <Paragraph hint className="mt-4 text-center lg:text-left">
-          Developers & SREs can simply execute Chaos Engineering with Litmus as
-          it is easy to use, based on modern chaos engineering practices &
-          community collaborated. Litmus is 100% open source & CNCF-hosted.
-        </Paragraph>
-        <div className="mt-4 flex gap-4 justify-center lg:justify-start">
-          <RegularButton href="https://github.com/litmuschaos/litmus" external>
-            <span className="flex items-center">
-              <GithubButton />
-              Get Started
-            </span>
-          </RegularButton>
-          <OutlinedButton href="/community">Get Involved</OutlinedButton>
-        </div>
-        <div className={styles.gitContainer}>
-          <GitHubButton
-            icon={"github_star"}
-            name={"Star"}
-            count={gitHub.stars}
-            href={"https://github.com/litmuschaos/litmus"}
-          />
-          <GitHubButton
-            icon={"github_watch"}
-            name={"Watch"}
-            count={gitHub.watchers}
-            href={"https://github.com/litmuschaos/litmus/subscription"}
-          />
-          <GitHubButton
-            icon={"github_fork"}
-            name={"Fork"}
-            count={gitHub.forks}
-            href={"https://github.com/litmuschaos/litmus/fork"}
+        <div className={styles.heroImg}>
+          <img
+            src="/landing_images/hero/hero_img.svg"
+            alt="LitmusChaos"
+            height={587}
+            width={596}
           />
         </div>
-      </div>
-      <div className={styles.heroImg}>
-        <img
-          src="/landing_images/hero/hero_img.svg"
-          alt="LitmusChaos"
-          height={646}
-          width={599}
-        />
-        {/* <video autoPlay muted playsInline loop>
-          <source src="/landing_images/hero/hero.mp4" />
-        </video> */}
-      </div>
-    </Container>
+      </Container>
+    </ContainerFluid>
   )
 }
 export default Hero
