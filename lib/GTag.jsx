0655231e438ff60df85599d2149d47b71f02ed17
Sahil <sahil@harness.io>
2023-06-29 16:33:56 +0530

    fix: updated google analytic tags
    Signed-off-by: Sahil <sahil@harness.io>

--- lib/GTag.jsx
+++ lib/GTag.jsx
@@ -1,5 +1,7 @@
 import Script from "next/script"
-const GTM_ID = "UA-92076314-26"
+const UA_ID = "UA-92076314-26"
+const GA4_ID = "G-3ZTFJCYCX9"
+
 const GTagScripts = () => {
   return (
     <>
@@ -10,7 +12,17 @@ const GTagScripts = () => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');`
+          })(window,document,'script','dataLayer','${UA_ID}');`
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GA4_ID}');`
         }}
       />
     </>
