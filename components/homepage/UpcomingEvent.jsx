217db315bff4db5125adc6232007a647742ad93a
Hrishav <hrishav.kumar@harness.io>
2023-11-02 12:33:15 +0530

    chore: Updated content and addded new adopters
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- components/homepage/UpcomingEvent.jsx
+++ components/homepage/UpcomingEvent.jsx
@@ -3,7 +3,6 @@ import { ContainerFluid, Container } from "@layouts/Container"
 import styles from "@includes/scss/UpcomingEvent.module.scss"
 import { Paragraph } from "@includes/Texts"
 import { TextLink } from "@includes/CTA"
-import { GetLocalTime } from "@lib/utils"
 
 const UpcomingEvent = () => {
   return (
@@ -22,19 +21,34 @@ const UpcomingEvent = () => {
             <div className={styles.topText}>
               <p className={styles.gradText}>UPCOMING COMMUNITY EVENT</p>
               <h3 className="text-xl sm:text-2xl md:text-3xl font-medium mt-2 text-primary">
-                KubeCon EU
+                KubeCon North America
               </h3>
               {/* <h4 className="text-base sm:text-lg md:text-xl font-medium text-primary mt-1 md:mt-4">
                 {GetLocalTime("18 Apr 2023 :30:00 +0530").dateString} @{" "}
                 {GetLocalTime("18 Apr 2023 16:30:00 +0530").time}
               </h4> */}
               <h4 className="text-base sm:text-lg md:text-xl font-medium text-primary mt-1 md:mt-4">
-                Apr 18<sup>th</sup> - Apr 21<sup>st</sup>, 2023
+                Nov 6<sup>th</sup> - 9<sup>th</sup>, 2023
               </h4>
+              <Paragraph>
+                <span className="mt-3 block font-semibold">
+                  Project Working Session
+                </span>{" "}
+                6th November (10:30 AM- 12:30 PM CST)
+                <br />
+                <span className="font-medium">Venue:</span> Hilton Garden Inn
+                McCormick Place - level 3 123 E. Cermak, Suite 300 Chicago, IL
+                60616
+                <br />
+                Connected to McCormick Place by skybridge. The skybridge at
+                McCormick Place is on level 3.
+                <br />
+                <span className="font-medium">Project Booth Number:</span> F20
+              </Paragraph>
             </div>
             <TextLink
               external
-              href="https://kccnceu2023.sched.com/event/1JWQB/litmuschaos-project-meeting?iframe=no&w=&sidebar=yes&bg=no"
+              href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/program/project-engagement/"
             >
               See Event Details
             </TextLink>
