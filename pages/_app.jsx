90d71c05709286147ebd913d42b560547bcf5b64
Hrishav <hrishav.kumar@harness.io>
2023-11-15 13:44:55 +0530

    chore: Added Scarf Pixel For Event Tracking (#80) (#81)
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,6 +11,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <GTagScripts />
+      <img
+        alt="scarf"
+        referrerPolicy="no-referrer-when-downgrade"
+        src="https://static.scarf.sh/a.png?x-pxid=a9abead1-4962-45b2-936a-730321bb7ec3"
+      />
       <TopNavbar key={router.asPath} />
       <Component {...pageProps} />
       <BottomFooter noPreFooter={noPreFooterPages.includes(router.asPath)} />
