17d1e3fa567ccd88ce27fd77891799a38103b43f
Hrishav <hrishav.kumar@harness.io>
2023-12-18 18:41:02 +0530

    chore: Updated community event (#82)
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- components/homepage/UpcomingEvent.jsx
+++ components/homepage/UpcomingEvent.jsx
@@ -3,6 +3,7 @@ import { ContainerFluid, Container } from "@layouts/Container"
 import styles from "@includes/scss/UpcomingEvent.module.scss"
 import { Paragraph } from "@includes/Texts"
 import { TextLink } from "@includes/CTA"
+import { GetLocalTime } from "@lib/utils"
 
 const UpcomingEvent = () => {
   return (
@@ -21,38 +22,18 @@ const UpcomingEvent = () => {
             <div className={styles.topText}>
               <p className={styles.gradText}>UPCOMING COMMUNITY EVENT</p>
               <h3 className="text-xl sm:text-2xl md:text-3xl font-medium mt-2 text-primary">
-                KubeCon North America
+                Build Resilience through Chaos
               </h3>
-              {/* <h4 className="text-base sm:text-lg md:text-xl font-medium text-primary mt-1 md:mt-4">
-                {GetLocalTime("18 Apr 2023 :30:00 +0530").dateString} @{" "}
-                {GetLocalTime("18 Apr 2023 16:30:00 +0530").time}
-              </h4> */}
               <h4 className="text-base sm:text-lg md:text-xl font-medium text-primary mt-1 md:mt-4">
-                Nov 6<sup>th</sup> - 9<sup>th</sup>, 2023
+                {GetLocalTime("24 Jan 2024 21:00:00 +0530").dateString} -{" "}
+                {GetLocalTime("25 Jan 2024 04:15:00 +0530").dateString}
               </h4>
               <Paragraph>
-                <span className="mt-3 block font-semibold">
-                  Project Working Session
-                </span>{" "}
-                6th November (10:30 AM- 12:30 PM CST)
-                <br />
-                <span className="font-medium">Venue:</span> Hilton Garden Inn
-                McCormick Place - level 3 123 E. Cermak, Suite 300 Chicago, IL
-                60616
-                <br />
-                Connected to McCormick Place by skybridge. The skybridge at
-                McCormick Place is on level 3.
-                <br />
-                <span className="block mt-2">
-                  <span className="font-bold mt-2">Project Booth Number:</span>{" "}
-                  F20
-                </span>
+                A global two-day virtual conference on all things Chaos
+                Engineering.
               </Paragraph>
             </div>
-            <TextLink
-              external
-              href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/program/project-engagement/"
-            >
+            <TextLink external href="https://chaoscarnival.io/">
               See Event Details
             </TextLink>
           </div>
