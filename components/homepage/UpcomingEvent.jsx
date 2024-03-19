415386d1228ed5772d2bb0939d027e4352097eed
Hrishav <hrishav.kumar@harness.io>
2024-03-19 14:13:27 +0530

    chore: Updated Community Event, Fixed Broken Link And Added New Blog (#92) (#93)
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- components/homepage/UpcomingEvent.jsx
+++ components/homepage/UpcomingEvent.jsx
@@ -22,18 +22,18 @@ const UpcomingEvent = () => {
             <div className={styles.topText}>
               <p className={styles.gradText}>UPCOMING COMMUNITY EVENT</p>
               <h3 className="text-xl sm:text-2xl md:text-3xl font-medium mt-2 text-primary">
-                Build Resilience through Chaos
+                KubeCon EU
               </h3>
               <h4 className="text-base sm:text-lg md:text-xl font-medium text-primary mt-1 md:mt-4">
-                {GetLocalTime("24 Jan 2024 21:00:00 +0530").dateString} -{" "}
-                {GetLocalTime("25 Jan 2024 04:15:00 +0530").dateString}
+                {GetLocalTime("19 Mar 2024 00:00:00 +0530").dateString} -{" "}
+                {GetLocalTime("22 Mar 2024 00:00:00 +0530").dateString}
               </h4>
-              <Paragraph>
-                A global two-day virtual conference on all things Chaos
-                Engineering.
-              </Paragraph>
+              <Paragraph>Project Booth No. PP2-A</Paragraph>
             </div>
-            <TextLink external href="https://chaoscarnival.io/">
+            <TextLink
+              external
+              href="https://events.linuxfoundation.org/kubecon-cloudnativecon-europe/"
+            >
               See Event Details
             </TextLink>
           </div>
