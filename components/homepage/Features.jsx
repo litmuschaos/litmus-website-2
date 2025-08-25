660048fa48a6ad5270a679f904a95f68d235d9e8
PriteshKiri <pritesh.d.kiri@gmail.com>
2025-08-25 22:49:56 +0530

    update: features section
    Signed-off-by: PriteshKiri <pritesh.d.kiri@gmail.com>

--- components/homepage/Features.jsx
+++ components/homepage/Features.jsx
@@ -7,11 +7,19 @@ import { featureUtils } from "./utils/featureUtils"
 const FeatureCard = ({ feature }) => {
   return (
     <div className={styles.featureCard}>
-      {feature.svg ?? ""}
-      <h3 className="text-xl md:text-2xl text-primary font-medium mt-4 mb-2">
-        {feature.title}
-      </h3>
-      <Paragraph hint>{feature.content}</Paragraph>
+      <div className={styles.featureContent}>
+        <h3 className="text-xl md:text-2xl font-medium mt-4 mb-2">
+          {feature.title}
+        </h3>
+        <Paragraph hint>{feature.content}</Paragraph>
+      </div>
+      <div className={styles.featureMascot}>
+        <img 
+          src={feature.mascot} 
+          alt={`${feature.title} mascot`}
+          className="w-full h-full object-contain"
+        />
+      </div>
     </div>
   )
 }
@@ -20,22 +28,27 @@ const Features = () => {
   return (
     <ContainerFluid className="bg-[#FAFBFC]">
       <Container className="md:pt-36 md:pb-28 pt-16 pb-14">
-        <BodyHead className="text-center md:text-left">
-          Loaded with features to{" "}
-          <span className="text-accent md:block">achieve resilience</span>
-        </BodyHead>
-        <Paragraph className="md:max-w-3xl mt-4 text-center md:text-left">
-          Over time, with the monthly cadence releases and community engagement,
-          we have added a lot of features and made LitmusChaos much easier for
-          the end-users. With the launch of Litmus 2.0, a new way of chaos
-          engineering can be performed by the users.
-        </Paragraph>
+        <div className="text-center">
+          <BodyHead className="text-center">
+          Packed with features to{" "}
+            <span className="text-[#5b44ba] md:block">achieve resilience</span>
+          </BodyHead>
+          <Paragraph className="md:max-w-2xl mt-4 text-center mx-auto">
+            Over time, with the monthly cadence releases and community engagement,
+            we have added a lot of features and made LitmusChaos much easier for
+            the end-users. With the launch of Litmus 2.0, a new way of chaos
+            engineering can be performed by the users.
+          </Paragraph>
+        </div>
         <div className={styles.featureCardCont}>
           {featureUtils.map((feature, index) => {
             return <FeatureCard key={feature.title + index} feature={feature} />
           })}
-          <div className={`${styles.featureCard} ${styles.cardBackImg}`}>
-            <h3 className="text-xl md:text-2xl text-success font-medium mt-4 mb-2">
+        </div>
+        
+        <div className={styles.moreFeatureCard}>
+          <div className={styles.content}>
+            <h3 className="text-xl md:text-2xl text-success font-medium mb-2">
               And many more features
             </h3>
             <TextLink
@@ -45,6 +58,7 @@ const Features = () => {
               View more features
             </TextLink>
           </div>
+          <div className={styles.mascot}></div>
         </div>
       </Container>
     </ContainerFluid>
